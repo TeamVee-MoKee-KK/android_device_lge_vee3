ba22757c528c22ac98df977064bea42ac3aec0dc
Caio Oliveira <caiooliveirafarias0@gmail.com>
2017-03-21 09:42:45 -0300

    vee3: Initial bringup for Mokee

--- mokee.mk
+++ mokee.mk
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/vee3/full_vee3.mk)
@@ -24,7 +24,7 @@ TARGET_SCREEN_HEIGHT := 320
 TARGET_SCREEN_WIDTH := 240
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_vee3
+PRODUCT_NAME := mk_vee3
 PRODUCT_RELEASE_NAME := L3II
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
